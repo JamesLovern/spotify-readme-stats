4d4491a6c7f7b900ade851cc8424d02beed28e63
james <james@lovern.io>
2021-05-25 22:43:26 +0100

    [Remove] Emojis

--- api/spotify.py
+++ api/spotify.py
@@ -95,14 +95,14 @@ def makeSVG(data):
 
     if data == {} or data["item"] == "None" or data["item"] is None:
         contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
-        currentStatus = "🎶 ~ Recently Listened to:"
+        currentStatus = "Recently Listened to:"
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
         itemIndex = random.randint(0, recentPlaysLength - 1)
         item = recentPlays["items"][itemIndex]["track"]
     else:
         item = data["item"]
-        currentStatus = "🎶 ~ Currently Playing:"
+        currentStatus = "Currently Playing:"
     
     if item["album"]["images"] == []:
         image = PLACEHOLDER_IMAGE
